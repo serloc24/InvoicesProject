0b0a95467555404bbfcbf390da8bfedc83a422d9
lopez <serlocol24@gmail.com>
2026-02-04 13:13:26 +0100

    First commit after Spring Initializr

---
+++ src/test/java/com/factures/FacturesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.factures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
